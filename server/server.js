2bafbd8d302bd8ac6be4899e2e934fa5dabfb85d
NusratAdor <nusratjahan141462@gmail.com>
2026-04-17 11:55:40 +0600

    Project completed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary();
 
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://freshmart-khaki.vercel.app']
 
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
